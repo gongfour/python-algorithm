2985a800601e296ce21692f31b53d655c7793017
wonjin <kangwonjin.dev@gmail.com>
2024-09-18 01:37:30 +0900

    solve 10768

---
+++ 10768.py
@@ -0,0 +1,15 @@
+
+m = int(input())
+d = int(input())
+
+if m == 2 and d == 18:
+    print('Special')
+elif m < 2:
+    print('Before')
+elif m >= 3:
+    print('After')
+elif m == 2 and d < 18:
+    print('Before')
+elif m == 2 and d > 18:
+    print('After')
+
